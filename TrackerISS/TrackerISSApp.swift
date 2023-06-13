8d40c215546e0c4ac909baf9be505487c0252ab4
Markel <markjuaristi@gmail.com>
2023-06-13 11:56:29 +0200

    comenzamos app de iOS para el rastreador de la localizacion de la estacion espacial internacional usando SwiftUI, combine y mapas 2D

---
+++ TrackerISS/TrackerISSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerISSApp.swift
+//  TrackerISS
+//
+//  Created by Markel Juaristi on 13/6/23.
+//
+
+import SwiftUI
+
+@main
+struct TrackerISSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
