c672ee929a0b94f44be2f72002a6a51bf2d4cc94
Markel <markjuaristi@gmail.com>
2023-06-14 18:55:07 +0200

    implemento mapviewmodel para traer las localizaciones

--- TrackerISS/Map/MapViewModel.swift
+++ TrackerISS/Map/MapViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import Combine
+
+class MapViewModel: ObservableObject {
+    private var cancellables = Set<AnyCancellable>()
+    private let apiService = APIService()
+
+    // Propiedades para almacenar los datos
+    @Published var issLocation: ISSLocationResponse?
+
+    func fetchISSLocation() {
+        apiService.fetchISSLocation()
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print("Error: \(error)")
+                }
+            } receiveValue: { [weak self] response in
+                self?.issLocation = response
+            }
+            .store(in: &cancellables)
+    }
+}
