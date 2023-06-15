185703e5ce327f05d256bc5c0fd67b661e34e358
Markel <markjuaristi@gmail.com>
2023-06-15 12:23:34 +0200

    modifico inicio de appa para que se ejecute correctamene la app

--- TrackerISS/TrackerISSApp.swift
+++ TrackerISS/TrackerISSApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct TrackerISSApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView(viewModel: LoginViewModel())
+            
         }
     }
 }
