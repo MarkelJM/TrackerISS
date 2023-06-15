b357111ea33967bf49318733370891178d2bacd4
Markel <markjuaristi@gmail.com>
2023-06-15 18:57:54 +0200

    añadimos funcion en el modelo de annotation para la comparativa del onChange del Map

--- TrackerISS/Map/Annotation/ISSAnnotationModel.swift
+++ TrackerISS/Map/Annotation/ISSAnnotationModel.swift
@@ -12,4 +12,14 @@ struct ISSAnnotation: Identifiable {
     // ISSAnnotation cumple con el protocolo Identifiable, que es necesario para usar con Map. Nuevas actualizaciones
     let id = UUID()
     var coordinate: CLLocationCoordinate2D
+    
+    // Para el .onChange del view:
+    // Esta función compara dos objetos de tipo ISSAnnotation.
+    // lhs (left hand side) es el objeto en el lado izquierdo del operador '=='
+    // rhs (right hand side) es el objeto en el lado derecho del operador '=='
+    // Aquí, decimos que dos objetos son iguales si sus latitudes y longitudes son iguales.
+    
+    static func ==(lhs: ISSAnnotation, rhs: ISSAnnotation) -> Bool {
+        return lhs.coordinate.latitude == rhs.coordinate.latitude && lhs.coordinate.longitude == rhs.coordinate.longitude
+    }
 }
--- TrackerISS/Map/MapView.swift
+++ TrackerISS/Map/MapView.swift
@@ -12,6 +12,14 @@ import MapKit
 struct MapView: View {
     @ObservedObject var viewModel = MapViewModel()
     
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(),
+        // Establecemos un span grande para ver más del mundo
+        span: MKCoordinateSpan(latitudeDelta: 180, longitudeDelta: 180)
+    )
+
+
+    
     var body: some View {
         Map(coordinateRegion: .constant(MKCoordinateRegion(center: viewModel.issAnnotations.first?.coordinate ?? CLLocationCoordinate2D(), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))), annotationItems: viewModel.issAnnotations) { annotation in
             MapAnnotation(coordinate: annotation.coordinate) {
@@ -21,6 +29,11 @@ struct MapView: View {
         .onAppear() {
             viewModel.fetchISSLocation()
         }
+        .onChange(of: viewModel.issAnnotations) { annotations in
+                    if let coordinate = annotations.first?.coordinate {
+                        region.center = coordinate
+                    }
+                }
     }
 }
 
